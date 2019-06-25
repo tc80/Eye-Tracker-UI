d478d49631e2f7759bdd5b6d2956da43b2646d3f
tc80 <tc80@st-andrews.ac.uk>
2019-06-25 00:36:01 -0400

    basic gui with selection

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,7 +1,76 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 final class GUI extends JFrame {
+
+    GUI() {
+        init();
+    }
+
+    private void init() {
+        JPanel panel = new JPanel(new GridLayout(1, 2));
+        List<JComponent> components = new ArrayList<JComponent>() {{
+            add(makeButton("YES"));
+            add(makeButton("NO"));
+        }};
+        components.forEach(panel::add);
+        add(panel);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(1000, 500);
+        setVisible(true);
+    }
+
+
+    private JButton makeButton(String s) {
+        JButton b = new JButton(s);
+        b.setFont(new Font("Arial", Font.PLAIN, 40));
+        b.addMouseListener(new MouseAdapter() {
+            Thread t;
+            boolean kill;
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                b.setText("FOCUSED");
+                b.setForeground(Color.RED);
+                kill = false;
+                t = new Thread(() -> {
+                    int secs = 0;
+                    while (true) {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (Exception ignored) {}
+                        if (kill) {
+                            break;
+                        }
+                        if (secs == 2) {
+                            b.setForeground(Color.GREEN);
+                            b.setText("SELECTED");
+                        }
+                        System.out.println(s + " FOCUSED FOR " + ++secs + " SECONDS");
+                    }
+                });
+                t.start();
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                b.setText(s);
+                b.setForeground(Color.BLACK);
+                kill = true;
+            }
+        });
+        return b;
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
 
 }
